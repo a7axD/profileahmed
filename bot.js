4238f12f47728d9969e6e78b6fde957e54a6608a
a7axD <44978645+a7axD@users.noreply.github.com>
2018-12-05 21:19:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "c";
-var adminprefix = 'c'
+var prefix = "p";
+var adminprefix = 'p'
 
-const developers = ["472413769700474901","id"]
+const developers = ["461778968459214859","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
